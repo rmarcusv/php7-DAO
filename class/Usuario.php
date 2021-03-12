c4b5b0ed9d4661c6772e1c5d783b0c7bb3800488
Marcus V Ribeiro <r.marcusv@gmail.com>
2021-03-11 23:01:16 -0300

    Metodos de lista e login

--- class/Usuario.php
+++ class/Usuario.php
@@ -53,6 +53,38 @@
 	    	}
 	    }
 
+	    public static function getList(){
+	    	$sql = new Sql(); // não utilizou o this, podendo então ser um método estático
+
+	    	return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	    }
+
+	    public static function search($login){
+	    	$sql = new Sql();
+	    	return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"));
+	    }
+
+	    public function login($login,$password){
+
+	    	$sql = new Sql();
+
+	    	$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+	    	if(count($results) > 0){
+	    		$row = $results[0];
+	    		$this->setIdusuario($row['idusuario']);	    	
+	    		$this->setDeslogin($row['deslogin']);
+	    		$this->setDessenha($row['dessenha']);
+	    		$this->setDtcadastro(new DateTime($row['dtcadastro']));
+	    	} else {
+
+	    		echo ("Login e/ou senha inválidos");
+	    	}
+	    }
+	  	
+	    
+	  	
 	    public function __toString(){
 	    	return json_encode(array(
 	    		"idusuario"=>$this->getIdusuario(),
--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 
 	require_once("config.php");
 
-	$root = new Usuario();
-
-	$root->loadbyId(1);
-
-	echo $root;
-	
 /*	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);*/
+	
+	// Carrega um único usuário
+/*	$root = new Usuario();
+	$root->loadbyId(1);
+	echo $root;*/
+	
+	// Carrega uma lista de usuarios
+/*	$lista = Usuario::getList(); // nã demanda instanciamento, por o método pertence à classe, e não à instância
+	echo json_encode($lista);*/
+
+	// Carrega uma lista de usuarios buscando pelo login/*
+	/*$search = Usuario::search("jo");
+	echo json_encode($search);*/
+
+	//login
+	/*$usuario = new Usuario();
+	$usuario->login("joao","122rty");
+	echo $usuario;*/
+
 
  ?>
